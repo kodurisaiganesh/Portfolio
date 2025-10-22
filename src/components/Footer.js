6c10d47f967e2db76399649c473cd92ec0d33a14
kodurisaiganesh <146118669+kodurisaiganesh@users.noreply.github.com>
2025-10-22 11:59:34 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/kodurisaiganesh"
+                href="https://www.instagram.com/_itsme_saii/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
